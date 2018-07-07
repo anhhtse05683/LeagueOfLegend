e049b3be245e9a62aa07aa7b6a161e8632629f57
kuroemon2509 <yokobang99@gmail.com>
2018-07-07 13:41:02 +0700

    Add files via upload

---
+++ Items/AttackDamageClass/AttackDamagePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LeagueOfLegend.Items.AttackDamageClass
+{
+    // This class stores necessary player info for out custom damage class, such as damage multiplierts and additions to knockback and crit.
+    public class AttackDamagePlayer : ModPlayer
+    {
+        public static AttackDamagePlayer ModPlayer(Player player)
+        {
+            return player.GetModPlayer<AttackDamagePlayer>();
+        }
+
+        // Vanilla only really has damage multiplers in code
+        // And crit and knockback is usually just added to
+        // As a modder, you could make separate variables for multipliers and simple addition bonuses
+        public float attackDamage = 1f;
+        public int attackBonus = 0;
+        public float attackKnockback = 0f;
+        public int attackCrit = 0;
+
+        public override void ResetEffects()
+        {
+            ResetVariables();
+        }
+
+        public override void UpdateDead()
+        {
+            ResetVariables();
+        }
+
+        private void ResetVariables()
+        {
+            attackDamage = 1f;
+            attackKnockback = 0f;
+            attackCrit = 0;
+        }
+    }
+}
---
+++ Items/AttackDamageClass/DuskbladeOfDraktharr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LeagueOfLegend.Items.AttackDamageClass
+{
+    public class DuskbladeOfDraktharr : AttackDamageItem
+    {
+        public const int PRICE = 2900;
+
+        public override void SetStaticDefaults()
+        {
+
+        }
+        public override void SafeSetDefaults()
+        {
+            item.damage = 55;
+            item.crit = 0;
+            item.knockBack = 0;
+            item.useStyle = 1;
+            item.useAnimation = 10;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.useTurn = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod.ItemType<Items.Gold>(), PRICE);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
